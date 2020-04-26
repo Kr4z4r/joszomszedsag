1054d077f26dbd9a6694336f24ad88d4f851df83
Kr4z4r <gerikomlo@gmail.com>
2020-04-26 13:43:03 +0200

    Added front english translations

--- resources/lang/en/front.php
+++ resources/lang/en/front.php
@@ -4,38 +4,37 @@ return [
   ////
   // partials/header.blade.php
   ////
-  "login" => "Bejelentkezés",
-
+  "login" => "Login",
+  
   ////
   // partials/footer.blade.php
   ////
-  "useful_links" => "Hasznos linkek",
-  "jvh_name" => "Jószomszédság Vigyázó Hálózata",
+  "useful_links" => "Useful links",
+  "jvh_name" => "Neighborhood Volunteer Network",
 
   ////
   // welcome.blade.php
   ////
   //  ->Introduction
-  "introduction_greeting" => "Drága Magyar Embertársak!",
-  "introduction_block_1" => "Sokunkban felmerül a kérdés, hogyha a jövőben unokáink megkérdeznek minket: nagypapa-nagymama Te mit tettél a koronavírus járvány idején? Akkor mit fogunk felelni? Én azt akarom majd mondani, hogy segítettem, amennyi az erőmből tellett. Se nem többet, se nem kevesebbet.",
-  "introduction_block_2" => "Érezzük, hogy nehéz időket élünk, valamit tennünk kell. Itt az idő, a lehetőség és egyben kötelesség, hogy megszervezzük a Jószomszédságok Vigyázó Hálózatát. Tegyünk a közelünkben lakó rászorulókért!!!",
-  "introduction_block_3" => "Mert senki sem maradhat egyedül.",
-  "ask_for_help" => "Kérjen segítséget!",
-  "if_you_need_help" => "Ha segítségre van szüksége, mert Ön idős, beteg, karanténban van vagy más problémája van, kérjen segítséget az Ön közelében élő Vigyázótól, aki felveszi Önnel a kapcsolatot.",
-  "request_help" => "Segítségkérés",
+  "introduction_greeting" => "Dear Visitor",
+  "introduction_block_1" => "Many of us wonder if in the future our grandchildren will ask us: grandpa-granda what did you do during the coronavirus epidemic? So what are we going to answer? I want to say that I helped as much as I could. No more, no less.",
+  "introduction_block_2" => "We are living through difficult times and we need to act now. It is our timely duty to organize the Neighborhood Volunteer Network. Let's do it for those who are in need, living near us!",
+  "introduction_block_3" => "Because no one can be left alone.",
+  "ask_for_help" => "Ask for help",
+  "if_you_need_help" => "If you need help because you are elderly, handicapped, ill, quarantined or have other problems, ask for help from a Volunteer nearby who will contact you.",
+  "request_help" => "Request help",
   //  ->Request help
-  "ask_for_help" => "Ask for help!",
-  "ask_for_help_description" => "If you need help, because you are elderly, sick or have any other problems, ask for help from the Guardians around you. They will get in contact with you.",
-  "request_help_button" => "Request Help",
+  "ask_for_help_description" => "Ask for help from a Volunteer near you location who will contact and help you.",
+  "request_help_button" => "Ask for help",
   //  ->Volunteer
-  "volunteer" => "Volunteer!",
-  "volunteer_desc" => "Regisztráljon Ön is Vigyázónak, ha segítséget nyújtana rászoruló szomszédainak lakókörzetében!",
-  "volunteer_button" => "Jelentkezés Vigyázónak",
+  "volunteer" => "Be a Volunteer",
+  "volunteer_desc" => "Register as a Volunteer to help people in your neighborhood!",
+  "volunteer_button" => "Be a Volunteer",
 
   ////
   // error_reporting.blade.php
   ////
-  "error_reporting_title" => "Hibabejelentés",
-  "send_error_report_button" => "Hiba beküldése"
+  "error_reporting_title" => "Report an error",
+  "send_error_report_button" => "Submit error report"
 
 ];
