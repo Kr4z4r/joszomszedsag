02dc52d94e2675abe66e8b40f953cad138cca711
adrian7 <adrian.silimon@yahoo.com>
2020-04-26 20:29:41 +0300

    Set phone verification timestamp fillable

--- app/User.php
+++ app/User.php
@@ -1,20 +1,21 @@
 <?php
-  
-  namespace App;
-  
-  use App\Notifications\VerifyEmail;
-  use Illuminate\Contracts\Auth\MustVerifyEmail;
-  use Illuminate\Foundation\Auth\User as Authenticatable;
-  use Illuminate\Notifications\Notifiable;
-  use App\Notifications\ResetPassword as ResetPassword;
-  use TCG\Voyager\Models\Role;
-  use TCG\Voyager\Models\User as VoyagerUser;
-  
-  class User extends VoyagerUser implements MustVerifyEmail
-  {
+
+namespace App;
+
+use TCG\Voyager\Models\Role;
+use App\Notifications\VerifyEmail;
+use Illuminate\Notifications\Notifiable;
+use TCG\Voyager\Models\User as VoyagerUser;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Notifications\ResetPassword as ResetPassword;
+
+class User extends VoyagerUser implements MustVerifyEmail {
+
     public $user_roles;
+
     use Notifiable;
-    
+
     /**
      * The attributes that are mass assignable.
      *
@@ -31,9 +32,10 @@
         'date_birth',
         'description',
         'facebook_profile',
-        'facebook_group'
+        'facebook_group',
+        'phone_verified_at'
     ];
-    
+
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -43,51 +45,51 @@
         'password',
         'remember_token',
     ];
-    
+
     /**
      * The attributes that should be cast to native types.
      *
      * @var array
      */
     protected $casts = [
-      'phone_verified_at' => 'datetime',
-      'email_verified_at' => 'datetime',
+        'phone_verified_at' => 'datetime',
+        'email_verified_at' => 'datetime',
     ];
-    
-    public function __construct(array $attributes = []) {
-      parent::__construct($attributes);
-      foreach (Role::all() as $role) {
-        $this->user_roles[$role->id] = $role->name;
-      }
+
+    public function __construct( array $attributes = [] ) {
+        parent::__construct( $attributes );
+        foreach ( Role::all() as $role ) {
+            $this->user_roles[ $role->id ] = $role->name;
+        }
     }
-    
-    public function sendPasswordResetNotification($token) {
-      $this->notify(new ResetPassword($token));
+
+    public function sendPasswordResetNotification( $token ) {
+        $this->notify( new ResetPassword( $token ) );
     }
+
     public function sendEmailVerificationNotification() {
-      $this->notify(new VerifyEmail());
+        $this->notify( new VerifyEmail() );
     }
-    
+
     public function canOpen() {
-      Role::all(['name']);
+        Role::all( [ 'name' ] );
     }
-    
-    
+
+
     public function getVolunteerDataByAddress() {
-      //$volunteer_data = App\UserVolunteerData::where('user_id' => )
+        //$volunteer_data = App\UserVolunteerData::where('user_id' => )
     }
 
-    protected static function boot()
-    {
-      parent::boot();
-      static::updating(function ($model) {
-        $old = self::find($model->id);
+    protected static function boot() {
+        parent::boot();
+        static::updating( function ( $model ) {
+            $old = self::find( $model->id );
 
-        logModelChange($old->attributes, $model->attributes,$model->id, 'Felhasználó: ');
-      });
+            logModelChange( $old->attributes, $model->attributes, $model->id, 'Felhasználó: ' );
+        } );
 
-      static::created(function ($model) {
-        saveLog($model->id, 'Új '.$model->role_id.' regisztrálva '.$model->email.' emaillel');
-      });
+        static::created( function ( $model ) {
+            saveLog( $model->id, 'Új ' . $model->role_id . ' regisztrálva ' . $model->email . ' emaillel' );
+        } );
     }
-  }
+}
